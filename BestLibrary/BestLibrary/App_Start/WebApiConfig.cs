dc5784c93889fe84ec62caad142157e192b0b1e1
Ruslan <r.j2012.rz@gmail.com>
2018-12-09 23:37:27 +0200

    Fix Bad Acc controller

--- BestLibrary/BestLibrary/App_Start/WebApiConfig.cs
+++ BestLibrary/BestLibrary/App_Start/WebApiConfig.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using Microsoft.Owin.Security.OAuth;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 
 namespace BestLibrary
 {
@@ -14,8 +9,8 @@ namespace BestLibrary
         {
             // Конфигурация и службы Web API
             // Настройка Web API для использования только проверки подлинности посредством маркера-носителя.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Маршруты Web API
             config.MapHttpAttributeRoutes();
